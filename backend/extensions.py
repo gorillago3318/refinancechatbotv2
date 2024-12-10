601211a018b7aaf336337e06c705a4fa679e872d
gorillago3318 <makwaikit@gmail.com>
2024-12-11 01:38:32 +0800

    Fully resolve merge conflict in backend/extensions.py

--- backend/extensions.py
+++ backend/extensions.py
@@ -1,15 +1,9 @@
-<<<<<<< HEAD
 # backend/extensions.py
 
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 # Initialize extensions without binding to the app
-=======
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
 
-# Initialize extensions
->>>>>>> 870f5012dd82d8904a3a273ae3903fd4423d0643
 db = SQLAlchemy()
 migrate = Migrate()
